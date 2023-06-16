0ee18930089c6fd584efe39fb206d7410fbcae0f
Anuj Jain <jainanuj744@gmail.com>
2023-06-16 17:40:39 +0530

    add modal functionality added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("I am imported");
+let add_btn = document.querySelector(".add-btn");
+let del_btn = document.querySelector(".del-btn");
+let modal = document.querySelector(".action");
+
+let addModal = true;
+
+add_btn.addEventListener("click",function(){
+    if(addModal){
+        modal.style.display = "flex";
+    }else{
+        modal.style.display = "none";
+    }
+    addModal = !addModal;
+})
+
+del_btn.addEventListener("click",function(){
+    console.log("Delete btn is clicked");
+})
