f3a11f7356ec297c61eeb5b216beef1ca6f22585
Anuj Jain <jainanuj744@gmail.com>
2023-06-19 21:05:20 +0530

    lock unlock working

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 let add_btn = document.querySelector(".add-btn");
 let del_btn = document.querySelector(".del-btn");
+let lock_btn = document.querySelector(".lock_btn i");
 let modal = document.querySelector(".action");
-
+let trash = document.querySelector(".fa-trash");
+// let lock_logo = lock_btn.querySelector(".fa-lock");
 let addModal = true;
+let deleteOnn = true;
+let lockFlag = true;
 
 add_btn.addEventListener("click",function(){
     if(addModal){
-        modal.style.display = "flex";
-    }else{
         modal.style.display = "none";
+    }else{
+        modal.style.display = "flex";
     }
     addModal = !addModal;
 })
 
 del_btn.addEventListener("click",function(){
-    console.log("Delete btn is clicked");
+    if(deleteOnn){
+        trash.style.color = "red";
+    }else{
+        trash.style.color = "black";
+    }
+    deleteOnn = !deleteOnn;
+})
+
+lock_btn.addEventListener("click",function(){
+    // console.log("lock btn clicked");
+    if(lock_btn.classList.contains("fa-lock")){
+        console.log("btn unlocked");
+        lock_btn.classList.remove("fa-lock");
+        lock_btn.classList.add("fa-lock-open");
+    }
+    else{
+        console.log("btn locked");
+        lock_btn.classList.remove("fa-lock-open");
+        lock_btn.classList.add("fa-lock");
+    }
 })
