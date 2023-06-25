14e8d1fafbc6ed3b4c0183aef03133b4723ae42b
Anuj Jain <jainanuj744@gmail.com>
2023-06-25 13:53:25 +0530

    card color working

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
 let add_btn = document.querySelector(".add-btn");
 let del_btn = document.querySelector(".del-btn");
-let lock_btn = document.querySelector(".lock_btn i");
+// let lock_btn = document.querySelector(".lock_btn i");
 let modal = document.querySelector(".action");
 let trash = document.querySelector(".fa-trash");
 let priority_color = document.querySelectorAll(".priority-color")
+let textarea = document.querySelector(".textarea");
+let card_container = document.querySelector(".card_container");
+
 // let lock_logo = lock_btn.querySelector(".fa-lock");
 let addModal = true;
-let deleteOnn = true;
+let deleteOnn = false;
+let cardColor = "black";
 // let lockFlag = true;
 
 add_btn.addEventListener("click", function () {
     if (addModal) {
-        modal.style.display = "none";
-    } else {
         modal.style.display = "flex";
+    } else {
+        modal.style.display = "none";
     }
     addModal = !addModal;
 })
 
 del_btn.addEventListener("click", function () {
     if (deleteOnn) {
-        trash.style.color = "red";
-    } else {
         trash.style.color = "black";
+    } else {
+        trash.style.color = "red";
     }
     deleteOnn = !deleteOnn;
 })
 
-lock_btn.addEventListener("click", function () {
-    if (lock_btn.classList.contains("fa-lock")) {
-        console.log("btn unlocked");
-        lock_btn.classList.remove("fa-lock");
-        lock_btn.classList.add("fa-lock-open");
-    }
-    else {
-        console.log("btn locked");
-        lock_btn.classList.remove("fa-lock-open");
-        lock_btn.classList.add("fa-lock");
-    }
-})
+// lock_btn.addEventListener("click", function () {
+//     if (lock_btn.classList.contains("fa-lock")) {
+//         console.log("btn unlocked");
+//         lock_btn.classList.remove("fa-lock");
+//         lock_btn.classList.add("fa-lock-open");
+//     }
+//     else {
+//         console.log("btn locked");
+//         lock_btn.classList.remove("fa-lock-open");
+//         lock_btn.classList.add("fa-lock");
+//     }
+// })
 
 for (let i = 0; i < priority_color.length; i++) {
-    priority_color[i].addEventListener("click", function () {
+    priority_color[i].addEventListener("click", function (e) {
         for (let i = 0; i < priority_color.length; i++) {
             if (priority_color[i].classList.contains("active")) {
                 priority_color[i].classList.remove("active");
             }
         }
         priority_color[i].classList.add("active");
+        cardColor = priority_color[i].classList[1];
     })
+}
+
+textarea.addEventListener("keydown", function (e) {
+    if(e.key == "Enter") {
+        let card_desc = textarea.value;
+        createCard(card_desc);
+        textarea.value = "";
+        modal.style.display = "none";
+        addModal = !addModal;
+    }
+})
+
+function createCard(card_desc) {
+    let cardContainer = document.createElement("div");
+    cardContainer.setAttribute("class","card");
+    cardContainer.innerHTML =`<div class="card-color ${cardColor}"></div>
+                              <div class="card_id">#123</div>
+                              <div class="card_area">${card_desc}</div>
+                              <div class="lock_btn"><i class="fa-solid fa-lock"></i></div>`
+    card_container.appendChild(cardContainer);
 }
