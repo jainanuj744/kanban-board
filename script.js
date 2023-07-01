4a4715fe873fbfabafe8a78ba25f7b0258ac1fde
Anuj Jain <jainanuj744@gmail.com>
2023-07-01 10:45:38 +0530

    local storge working

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ let color = ["red", "blue", "green", "black"];
 
 var uid = new ShortUniqueId();
 
+let cardArr = [];
+
+if (localStorage.getItem("tickets")) {
+    let str = localStorage.getItem("tickets");
+    let arr = JSON.parse(str);
+    console.log(arr);
+    cardArr = arr;
+    for (let i = 0; i < cardArr.length; i++) {
+        let cardObj = cardArr[i];
+        createCard(cardObj.desc, cardObj.color, cardObj.id);
+    }
+}
+
+//filtering the card
 for (let i = 0; i < filter.length; i++) {
     filter[i].addEventListener("click", function () {
         let cardCol = document.querySelectorAll(".card-color");
@@ -68,34 +82,45 @@ for (let i = 0; i < priority_color.length; i++) {
 textarea.addEventListener("keydown", function (e) {
     if (e.key == "Enter") {
         let card_desc = textarea.value;
-        createCard(card_desc);
+        createCard(card_desc, cardColor);
         textarea.value = "";
         modal.style.display = "none";
         addModal = !addModal;
     }
 })
 
-function createCard(card_desc) {
+function createCard(card_desc, cardColor, cardId) {
+    let id;
+    if (cardId == undefined) {
+        id = uid();
+    } else {
+        id = cardId;
+    }
     let cardContainer = document.createElement("div");
     cardContainer.setAttribute("class", "card");
     cardContainer.innerHTML = `<div class="card-color ${cardColor}"></div>
-                              <div class="card_id">#${uid()}</div>
+                              <div class="card_id">#${id}</div>
                               <div class="card_area">${card_desc}</div>
                               <div class="lock_btn"><i class="fa-solid fa-lock"></i></div>`
+    if (cardId == undefined) {
+        cardArr.push({ color: cardColor, id: id, desc: card_desc });
+        // console.log(cardArr);
+        updateLocalStorage();
+    }
     card_container.appendChild(cardContainer);
 
     // handle lock-unlock
-    handleLockUnlock(cardContainer);
+    handleLockUnlock(cardContainer, id);
 
     // handle delete
     handleDelete(cardContainer);
 
     // handle priority color
-    handlePriorityColor(cardContainer);
+    handlePriorityColor(cardContainer, id);
 
 }
 
-function handleLockUnlock(cardContainer) {
+function handleLockUnlock(cardContainer, id) {
     let lock_btn = document.querySelector(".lock_btn i");
     let card_area = cardContainer.querySelector(".card_area");
     lock_btn.addEventListener("click", function () {
@@ -109,6 +134,11 @@ function handleLockUnlock(cardContainer) {
             lock_btn.classList.add("fa-lock");
             card_area.setAttribute("contenteditable", "false");
         }
+
+        let card_index = getCardIdx(id);
+        cardArr[card_index].desc = card_area.textContent;
+        // updateLocalStorage();
+        // console.log(cardArr);
     })
 }
 
@@ -117,10 +147,11 @@ function handleDelete(cardContainer) {
         if (deleteOnn) {
             cardContainer.remove();
         }
+        updateLocalStorage();
     })
 }
 
-function handlePriorityColor(cardContainer) {
+function handlePriorityColor(cardContainer, id) {
     let card_color = cardContainer.querySelector(".card-color");
     card_color.addEventListener("click", function () {
         let current_color = card_color.classList[1];
@@ -131,5 +162,22 @@ function handlePriorityColor(cardContainer) {
         card_color.classList.remove(current_color);
         card_color.classList.add(next_color);
 
+        let card_index = getCardIdx(id);
+        cardArr[card_index].color = next_color;
+        updateLocalStorage();
+        // console.log(cardArr);
     })
+}
+
+function getCardIdx(id) {
+    for (let i = 0; i < cardArr.length; i++) {
+        if (cardArr[i].id == id) {
+            return i;
+        }
+    }
+}
+
+function updateLocalStorage() {
+    let stingifyCardArr = JSON.stringify(cardArr);
+    localStorage.setItem("tickets", stingifyCardArr);
 }
